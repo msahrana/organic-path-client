97f0c43b134fe39891189f07057219daeb39b647
msahrana <msahrana@gmail.com>
2024-08-16 08:38:46 +0600

    contact us section is add

--- src/routes/Routes/Routes.jsx
+++ src/routes/Routes/Routes.jsx
@@ -1,33 +1,33 @@
-// import {createBrowserRouter} from "react-router-dom";
-// import Root from "../../layout/Root/Root";
-// import ErrorPage from "../../error/ErrorPage/ErrorPage";
-// import Home from "../../components/Home/Home";
-// import ContactUs from "../../components/ContactUs/ContactUs";
-// import Register from "../../components/Register/Register";
-// import Login from "../../components/Login/Login";
+import {createBrowserRouter} from "react-router-dom";
+import Root from "../../layout/Root/Root";
+import ErrorPage from "../../error/ErrorPage/ErrorPage";
+import Home from "../../components/Home/Home";
+import ContactUs from "../../components/ContactUs/ContactUs";
+import Register from "../../components/Register/Register";
+import Login from "../../components/Login/Login";
 
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root></Root>,
-//     errorElement: <ErrorPage></ErrorPage>,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home></Home>,
-//       },
-//       {
-//         path: "/contactUs",
-//         element: <ContactUs></ContactUs>,
-//       },
-//       {
-//         path: "/register",
-//         element: <Register></Register>,
-//       },
-//       {
-//         path: "/login",
-//         element: <Login></Login>,
-//       },
-//     ],
-//   },
-// ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/contactUs",
+        element: <ContactUs></ContactUs>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
